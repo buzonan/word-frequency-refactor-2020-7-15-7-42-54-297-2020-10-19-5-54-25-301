cf1d5e9d1bda978bd32d937a2033f30a1c9ae1aa
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-19 15:57:34 +0800

    [word-frequency][andrey]refactor: extract calculate word frequency logic to calculateWordFrequency method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -15,22 +15,7 @@ public class WordFrequencyGame {
 
             try {
 
-                String[] words = sentence.split(WORDS);
-
-                List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String word : words) {
-                    WordInfo wordInfo = new WordInfo(word, 1);
-                    wordInfoList.add(wordInfo);
-                }
-
-                Map<String, List<WordInfo>> wordInfoMap =getListMap(wordInfoList);
-
-                List<WordInfo> distinctWordInfos = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    distinctWordInfos.add(wordInfo);
-                }
-                wordInfoList = distinctWordInfos;
+                List<WordInfo> wordInfoList = calculateWordFrequency(sentence);
 
                 wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
@@ -44,6 +29,26 @@ public class WordFrequencyGame {
                 return CALCULATE_ERROR;
             }
         }
+    }
+
+    private List<WordInfo> calculateWordFrequency(String sentence) {
+        String[] words = sentence.split(WORDS);
+
+        List<WordInfo> wordInfoList = new ArrayList<>();
+        for (String word : words) {
+            WordInfo wordInfo = new WordInfo(word, 1);
+            wordInfoList.add(wordInfo);
+        }
+
+        Map<String, List<WordInfo>> wordInfoMap =getListMap(wordInfoList);
+
+        List<WordInfo> distinctWordInfos = new ArrayList<>();
+        for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
+            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+            distinctWordInfos.add(wordInfo);
+        }
+        wordInfoList = distinctWordInfos;
+        return wordInfoList;
     }
 
 
